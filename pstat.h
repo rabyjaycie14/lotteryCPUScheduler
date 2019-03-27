0cf851198727663c5e3d12424183abaf98f740e4
Matt Kohls <mattkohls13@gmail.com>
2019-03-27 17:34:26 -0400

    settickets syscall stuff i hope

---
+++ pstat.h
@@ -0,0 +1,13 @@
+#ifndef _PSTAT_H_
+#define _PSTAT_H_
+
+#include "param.h"
+
+struct pstat {
+	int num_processes; // the total number of non-UNUSED processes in the process table
+	int pid[NPROC];    // the PID of each process
+	int tickets[NPROC];// the number of tickets this process has
+	int ticks[NPROC];  // the number of ticks each process has accumulated
+};
+
+#endif // _PSTAT_H_
